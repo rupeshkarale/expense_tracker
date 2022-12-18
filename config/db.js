22419b1e70a116615cc4d2f9e8b958e5ba1696fc
Rupesh Karale <rupeshkarale3245@gmail.com>
2022-12-18 17:19:37 +0530

    live link added

--- config/db.js
+++ config/db.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
-// require("dotenv").config();
+require("dotenv").config();
 
 const dbID = process.env.DB_ID;
 const dbPassword = process.env.DB_PASSWORD;
 
 const connection = mongoose.connect(
-  // `mongodb+srv://${dbID}:${dbPassword}@cluster0.6lt3yah.mongodb.net/?retryWrites=true&w=majority`
-  "mongodb://127.0.0.1:27017/expensesTracker"
+
+  `mongodb+srv://expenseTracker:${dbPassword}@cluster0.yzilnkf.mongodb.net/?retryWrites=true&w=majority`
+
+  //use below link if you want run on localhost
+  // "mongodb://127.0.0.1:27017/expensesTracker"
 );
 
 module.exports = connection;
