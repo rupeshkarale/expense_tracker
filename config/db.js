8aec55fc23bbe57e061591cf694d071a60d47916
Rupesh Karale <rupeshkarale3245@gmail.com>
2022-12-20 15:50:42 +0530

    mongo url added

--- config/db.js
+++ config/db.js
@@ -3,8 +3,9 @@ require("dotenv").config();
 
 const dbID = process.env.DB_ID;
 const dbPassword = process.env.DB_PASSWORD;
-
-const connection = mongoose.connect("mongodb://127.0.0.1:27017/expensesTracker");
+mongoose.set("strictQuery", false);
+// const connection = mongoose.connect("mongodb://127.0.0.1:27017/expensesTracker");
+const connection = mongoose.connect(`mongodb+srv://expenseTracker:${dbPassword}@cluster0.yzilnkf.mongodb.net/?retryWrites=true&w=majority`)
 
 module.exports = connection;
 
